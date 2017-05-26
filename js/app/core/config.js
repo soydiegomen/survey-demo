01c60bce8e91a366482c60d5e3cef03a61a82c97
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-05-25 22:47:49 -0500

    Modificación de un archivo de configuración

--- js/app/core/config.js
+++ js/app/core/config.js
@@ -16,6 +16,20 @@
         recommendationQuestion : '591facd4eba7ce1d04000006'
     });
 
+    /*
+    core.constant('appConfig', 
+    {
+        maxMonths : 6,
+        //TODO: Change id for code. Will be more easy for setup
+        surveyId : '5927a0f9a8d5b8ac41000017',
+        commentQuestion : '5927a0f9a8d5b8ac41000018',
+        generalScoreQue : '5927a0f9a8d5b8ac4100001c',
+        priceQuestion : '5927a0f9a8d5b8ac4100001b',
+        staffQuestion : '5927a0f9a8d5b8ac4100001a',
+        recommendationQuestion : '5927a0f9a8d5b8ac41000019'
+    });
+    */
+
     core.config(configure);
 
     //configure.$inject = ['$routeProvider', 'routehelperConfigProvider', 'locationProvider'];
